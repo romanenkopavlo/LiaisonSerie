225a587e7a0123da1f97a5f05eb00192322ab1e4
utopiaforever <pavlo.romanenko@lyceeastier.com>
2024-01-21 12:55:51 +0100

    La version definitive

--- src/main/java/fr/btsciel/Main.java
+++ src/main/java/fr/btsciel/Main.java
@@ -7,15 +7,11 @@ import java.nio.charset.StandardCharsets;
 public class Main {
     public static void main(String[] args) {
         Application app = new Application();
-        String choice;
         app.listerLesPorts().forEach(com->System.out.println(com));
-        System.out.println("GO or IEEE?");
-        System.out.print("Your choice: ");
-        choice = In.readString().toUpperCase();
         try {
             while (true) {
                 app.initialisation("COM10");
-                app.ecrire(choice.getBytes(StandardCharsets.US_ASCII));
+                app.ecrire("GO".getBytes(StandardCharsets.US_ASCII));
                 app.ecrire("\r".getBytes(StandardCharsets.US_ASCII));
                 if (app.detecteSiReception() > 0) {
                     System.out.println(new String(app.lireTrame(app.detecteSiReception())));
